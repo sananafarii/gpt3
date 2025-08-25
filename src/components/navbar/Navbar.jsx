b6853c4e770b25c5ed80a27f896f49f497446350
sana <sananafari1382@gmail.com>
2025-08-25 15:45:07 +0330

    feat:

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import logo from './assets/logo.svg';
 import './navbar.css'
+
+//BEM -> block elements modifier
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo}alt="logo" />
+
+        </div>
+      </div>
     </div>
   )
 }
