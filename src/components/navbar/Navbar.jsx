c28ee9b30d1b3640238efccd58d644c82fcbf0c7
sana <sananafari1382@gmail.com>
2025-08-28 19:45:29 +0330

    build:

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
-import './navbar.css'
+import './navbar.css';
 
-//BEM -> block elements modifier
+
+const Menu = () => (
+  <>
+      <p><a href="#home">Home</a></p>
+      <p><a href="#wgpt3">What is GPT3?</a></p>
+      <p><a href="#possibility">Open AI</a></p>
+      <p><a href="#features">Case Studies</a></p>
+      <p><a href="#blog">Blog</a></p>
+  </>
+)
+
+
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false); 
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -13,11 +25,7 @@ const Navbar = () => {
           <img src={logo}alt="logo" />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Blog</a></p>
+         <Menu />
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
@@ -25,6 +33,21 @@ const Navbar = () => {
         <button type='button'>Sign up</button>
       </div>
       <div className='gpt3__navbar-menu'>
+        { toggleMenu 
+         ? <RiCloseLin color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+         : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+      { toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <Menu />
+          </div>
+          <div className='gpt3__navbar-menu_container-sign'>
+            <p>Sign in</p>
+            <button type='button'>Sign up</button>
+          </div>
+        </div>
+      )} 
       </div>
     </div>
   )
